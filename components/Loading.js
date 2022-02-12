a39e3695c034a67f6b62001f63819d8d407a40c9
Vivek <dev.vivekkhanal@gmail.com>
2022-02-12 14:46:09 +0530

    fixed className error in Loading Component

--- components/Loading.js
+++ components/Loading.js
@@ -9,8 +9,8 @@ function Loading() {
       </Head>
       <div className="bg-[#111b21] text-white h-screen w-full grid place-items-center">
         {/* <TailSpin color="#22c55e" height={80} width={80} /> */}
-        <div class="border-[#22c55e] animate-spin inline-block w-[50px] h-[50px] border-4 rounded-full">
-          <div class="text-[#111b21] rounded-full bg-[#111b21] w-[30px] h-[20px]  rotate-45 "></div>
+        <div className="border-[#22c55e] animate-spin inline-block w-[50px] h-[50px] border-4 rounded-full">
+          <div className="text-[#111b21] rounded-full bg-[#111b21] w-[30px] h-[20px]  rotate-45 "></div>
         </div>
       </div>
     </>
